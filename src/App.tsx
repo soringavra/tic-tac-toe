8895bac7e6a878ce556eb158b7e9c0dca0a79ab1
Zorinul <soringvr2@gmail.com>
2025-03-14 00:31:32 +0200

    Added score goal support

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ const App = () => {
   const [winner, setWinner] = useState<string | null>(null);
   const [winningLine, setWinningLine] = useState<number[] | null>(null);
   const [isDraw, setIsDraw] = useState(false);
+  const [scoreGoal, setScoreGoal] = useState(10);
+  // const [timer, setTimer] = useState(0);
   
   const handleClick = (i: number) => {
     if(board[i] || winningLine)
@@ -56,11 +58,13 @@ const App = () => {
   return (
     <div className="max-w-[96rem] h-full grid place-items-center m-auto">
       <div className="flex flex-col gap-3">
-        <p className={`${(winningLine || isDraw) ? "visible" : "invisible"} text-white font-semibold text-center mb-2`}>{isDraw ? "It's a draw!" : (isXNext ? "O won!" : "X won!")}</p>
-        <Scoreboard score={score} isXNext={isXNext} isDraw={isDraw} winner={winner} />
+        <p className={`${(winningLine || isDraw) ? "visible" : "invisible"} text-white font-semibold text-center mb-2`}>{isDraw ? "It's a draw!" : (isXNext ? "O won" : "X won")}{(score.X == scoreGoal || score.O == scoreGoal) ? " the game" : " the round"}!</p>
+        <Scoreboard score={score} isXNext={isXNext} isDraw={isDraw} winner={winner} scoreGoal={scoreGoal} />
         <Table board={board} onClick={handleClick} isXNext={isXNext} winningLine={winningLine} />
         <div className={`${(winningLine || isDraw) ? "visible" : "invisible"} flex gap-3`}>
-          <button onClick={resetGame} className="text-white bg-blue-600 hover:bg-blue-500 flex-2 border-blue-500">Play Again</button>
+          {(score.X != scoreGoal && score.O != scoreGoal) && (
+            <button onClick={resetGame} className="text-white bg-blue-600 hover:bg-blue-500 flex-2 border-blue-500">Play Again</button>
+          )}
           <button onClick={() => { resetGame(); setScore({ X: 0, O: 0 }); }} className="text-white bg-slate-700 hover:bg-slate-600 flex-1 border-slate-600">Reset</button>
         </div>
       </div>
--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -3,19 +3,21 @@ const Scoreboard = ({
   isXNext,
   isDraw,
   winner,
+  scoreGoal,
 }: {
   score: { X: number, O: number },
   isXNext: boolean,
   isDraw: boolean,
   winner: string | null,
+  scoreGoal: number,
 }) => {
   return (
     <div className="text-white font-xo tracking-widest flex gap-3">
-      <div className={`${!winner && !isDraw ? (isXNext ? "outline-2 outline-blue-400" : "outline-0") : winner == "X" && !isDraw ? "outline-2 outline-green-400" : isDraw && "outline-0"} bg-slate-700 flex p-4 border border-slate-600 outline-offset-2 rounded-lg`}>
+      <div className={`${!winner && !isDraw ? (isXNext ? "outline-2 outline-blue-400" : "outline-0") : winner == "X" && !isDraw ? "outline-2 outline-green-400" : isDraw && "outline-0"} ${score.O == scoreGoal && "opacity-50"} bg-slate-700 flex p-4 border border-slate-600 outline-offset-2 rounded-lg`}>
         <h1 className="text-blue-400 text-3xl">X</h1>
         <h1 className="w-16 text-3xl text-end">{score.X.toString().padStart(2, "0")}</h1>
       </div>
-      <div className={`${!winner && !isDraw ? (!isXNext ? "outline-2 outline-red-400" : "outline-0") : winner == "O" && !isDraw ? "outline-2 outline-green-400" : isDraw && "outline-0"} bg-slate-700 flex p-4 border border-slate-600 outline-offset-2 rounded-lg`}>
+      <div className={`${!winner && !isDraw ? (!isXNext ? "outline-2 outline-red-400" : "outline-0") : winner == "O" && !isDraw ? "outline-2 outline-green-400" : isDraw && "outline-0"} ${score.X == scoreGoal && "opacity-50"} bg-slate-700 flex p-4 border border-slate-600 outline-offset-2 rounded-lg`}>
         <h1 className="w-16 text-3xl">{score.O.toString().padStart(2, "0")}</h1>
         <h1 className="text-red-400 text-3xl">O</h1>
       </div>
