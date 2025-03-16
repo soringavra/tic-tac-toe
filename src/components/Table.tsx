56145b28d41b8318788091cc603e38ae2da35488
Zorinul <soringvr2@gmail.com>
2025-03-16 10:24:39 +0200

    Removed the win line; Winner cells are highlighted instead

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,3 @@
-import { getLinePosition } from "../utils";
-
 import Cell from "./Cell";
 
 const Table = ({
@@ -14,8 +12,7 @@ const Table = ({
   winningLine: number[] | null,
 }) => {
   return (
-    <div className="bg-slate-700 grid grid-cols-3 border border-slate-600 rounded-lg relative">
-      {winningLine && <div className={`${getLinePosition(winningLine)} ${isXNext ? "bg-red-500/50 outline-red-500" : "bg-blue-500/50 outline-blue-500"} h-12 z-10 outline rounded-full absolute -translate-x-1/2 -translate-y-1/2`} />}
+    <div className="bg-slate-700 grid grid-cols-3 overflow-hidden border border-slate-600 rounded-lg relative">
       {board.map((cell, i) => (
         <Cell
           key={i}
